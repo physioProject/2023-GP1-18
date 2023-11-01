52ff1128691e6ec82e61edf0caf9ba1e31e7e13d
lujain-1 <128541474+lujain-1@users.noreply.github.com>
2023-11-02 01:43:55 +0300

    Update AppConstants.dart

--- lib/Widget/AppConstants.dart
+++ lib/Widget/AppConstants.dart
@@ -3,6 +3,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class AppConstants {
   static CollectionReference userCollection =
       FirebaseFirestore.instance.collection('users');
+  static CollectionReference exerciseCollection =
+  FirebaseFirestore.instance.collection('exercises');
 
   static String typeIsTherapist = 'therapist';
   static String typeIsPatient = 'patient';
@@ -17,6 +19,14 @@ class AppConstants {
     'Carpel tunnel',
     'Shoulder impingement syndrome',
     'Shoulder recurrent dislocation'
+  ];
+  static List<String> ExerciseList = [
+    'ex1 level1',
+    'ex1 level2',
+    'ex2 level1',
+    'ex2 level2',
+    'ex3 level1',
+    'ex3 level2'
   ];
   static List<String> typeMenu = ['admin', 'patient', 'therapist'];
 }
