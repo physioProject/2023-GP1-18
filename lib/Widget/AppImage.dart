d57a6e52c2076ef982099c93442d2e73a14265b1
lujain-1 <128541474+lujain-1@users.noreply.github.com>
2023-11-30 19:18:54 +0300

    Update AppImage.dart

--- lib/Widget/AppImage.dart
+++ lib/Widget/AppImage.dart
@@ -1,5 +1,6 @@
 class AppImage{
  static String backgroundImage='assets/image/logo.jpeg';
   static String  profile='assets/image/pic.png';
- static String  exercise='assets/image/assets/image/photo1700659155.jpeg';
+ static String  exercise='assets/image/exercise.jpg';
+
 }
