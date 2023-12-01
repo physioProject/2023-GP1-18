818390d01adb7952f13a62d3c2307da35ee45bee
lujain-1 <128541474+lujain-1@users.noreply.github.com>
2023-12-01 14:00:38 +0300

    Update Login.dart

--- lib/Screens/Account/Login.dart
+++ lib/Screens/Account/Login.dart
@@ -1,133 +1,226 @@
-import 'package:email_validator/email_validator.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:physio/Widget/AppButtons.dart';
-import 'package:physio/Widget/AppTextFields.dart';
-import '../../Widget/AppBar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:physio/Screens/Account/ForgotPassword.dart';
+import 'package:physio/Screens/Admin/AdminHome.dart';
+import 'package:physio/Widget/AppImage.dart';
+import 'package:physio/Widget/AppRoutes.dart';
+import '../../Database/Database.dart';
+import '../../Widget/AppButtons.dart';
+import '../../Widget/AppColor.dart';
+import '../../Widget/AppConstants.dart';
+import '../../Widget/AppDropList.dart';
 import '../../Widget/AppLoading.dart';
 import '../../Widget/AppMessage.dart';
+import '../../Widget/AppTextFields.dart';
 import '../../Widget/AppValidator.dart';
-class ForgotPassword extends StatefulWidget {
-  const ForgotPassword({Key? key}) : super(key: key);
+import '../Patient/PatientHome.dart';
+import '../Therapist/ViewPatientList.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
 
   @override
-  State<ForgotPassword> createState() => _ForgotPasswordState();
+  State<Login> createState() => _LoginState();
 }
 
-class _ForgotPasswordState extends State<ForgotPassword> {
-  final emailController = TextEditingController();
+class _LoginState extends State<Login> {
+  late Image bcImage;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  GlobalKey<FormState> logKey = GlobalKey();
+  String? selectedType;
+
+  var patientId;
+  @override
+  void initState() {
+    super.initState();
+    WidgetsFlutterBinding.ensureInitialized();
+    bcImage = Image.asset(AppImage.backgroundImage);
+  }
 
   @override
-  void dispose() {
-    emailController.dispose();
-    super.dispose();
-  }
-
-  Future<void> passReset() async {
-    final email = emailController.text.trim();
-
-//=============================Check if the user is registered===============================
-    try {
-      if (await isEmailRegistered(email)) {
-        await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
-        AppLoading.show(
-            context, AppMessage.sendEmail, AppMessage.done);
-      } else {
-        AppLoading.show(
-            context, AppMessage.notsendEmail, AppMessage.userNotFound);
-      }
-    }
-    //===========================Catch other exceptions =============================
-    on FirebaseAuthException catch (e) {
-      showAlertDialog('Error: ${e.message}');
-    } catch (e) {
-      showAlertDialog('An unexpected error occurred.');
-    }
-  }
-
-  bool isEmailValid(String email) {
-    return EmailValidator.validate(email);
-  }
-
-  Future<bool> isEmailRegistered(String email) async {
-    QuerySnapshot<Map<String, dynamic>> querySnapshot = await FirebaseFirestore.instance
-        .collection('users')
-        .where('email', isEqualTo: email)
-        .get();
-
-    return querySnapshot.docs.isNotEmpty;
-  }
-
-  void showSnackBar(String message) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text(message),
-        backgroundColor: Colors.red,
-      ),
-    );
-  }
-
-  void showAlertDialog(String message) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          content: Text(message),
-        );
-      },
-    );
-  }
-
-  void showSuccessDialog(String message) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          content: Text(message),
-        );
-      },
-    );
+  void didChangeDependencies() {
+    precacheImage(bcImage.image, context);
+    super.didChangeDependencies();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarWidget(text: 'Forgot Password'),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(height: 10),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: Text(
-              'Please enter your email so we can send you a reset link',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 20),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            image: DecorationImage(
+                image: bcImage.image,
+                colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.3), BlendMode.darken),
+                fit: BoxFit.cover)),
+        child: Form(
+          key: logKey,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //==============================Type Menu===============================================================
+                AppDropList(
+                  listItem: AppConstants.typeMenu,
+                  validator: (v) {
+                    if (v == null) {
+                      return AppMessage.mandatoryTx;
+                    } else {
+                      return null;
+                    }
+                  },
+                  onChanged: (selectedItem) {
+                    selectedType = selectedItem;
+                  },
+                  hintText: AppMessage.type,
+                  dropValue: selectedType,
+                  fillColor: AppColor.opacityFillColor,
+                ),
+                SizedBox(
+                  height: 20.h,
+                ),
+
+//==============================Email===============================================================
+                AppTextFields(
+                  controller: emailController,
+                  labelText: AppMessage.emailTx,
+                  validator: (v) => AppValidator.validatorEmail(v),
+                  obscureText: false,
+                  fillColor: AppColor.opacityFillColor,
+                ),
+                SizedBox(
+                  height: 10.h,
+                ),
+//==============================Password===============================================================
+                AppTextFields(
+                  controller: passwordController,
+                  labelText: AppMessage.passwordTx,
+                  validator: (v) => AppValidator.validatorPassword(v),
+                  obscureText: true,
+                  fillColor: AppColor.opacityFillColor,
+                ),
+                SizedBox(
+                  height: 10.h,
+                ),
+//////==============================Forgot password ========================================================
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return const ForgotPassword();
+                              },
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          'Forgot password?',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 10.h,
+                ),
+
+//===============================Add Button===============================================================
+                AppButtons(
+                  text: AppMessage.loginTx,
+                  bagColor: AppColor.iconColor,
+                  onPressed: () async {
+                    FocusManager.instance.primaryFocus?.unfocus();
+                    if (logKey.currentState?.validate() == true) {
+                      AppLoading.show(context, '', 'lode');
+                      Database.loggingToApp(
+                        email: emailController.text.trim(),
+                        password: passwordController.text,
+                      ).then((v) {
+                        if (v == 'error') {
+                          Navigator.pop(context);
+                          AppLoading.show(
+                              context, AppMessage.loginTx, AppMessage.error);
+                        } else if (v == 'user-not-found') {
+                          Navigator.pop(context);
+                          AppLoading.show(context, AppMessage.loginTx,
+                              AppMessage.userNotFound);
+                        } else {
+                          FirebaseFirestore.instance
+                              .collection('users')
+                              .where('userId', isEqualTo: v)
+                              .get()
+                              .then((typeFromDB) {
+                            Navigator.pop(context);
+                            for (var element in typeFromDB.docs) {
+                              print('name is: ${element.data()['name']}');
+                              print(element.data()['type'] == selectedType);
+                              if (element.data()['type'] == selectedType &&
+                                  element.data()['activeUser'] == true) {
+                                if (element.data()['type'] ==
+                                    AppConstants.typeIsPatient) {
+                                  String patientId = element.data()['userId'];
+                                  AppRoutes.pushReplacementTo(
+                                      context,
+                                      PatientHome(
+                                        name: element.data()['firstName'] +
+                                            ' ' +
+                                            element.data()['lastName'],
+                                           patientId: patientId,
+
+                                  ));
+                                } else if (element.data()['type'] ==
+                                    AppConstants.typeIsTherapist) {
+                                  if (selectedType ==
+                                      AppConstants.typeIsTherapist) {
+                                    String therapistId =
+                                    element.data()['userId'];
+                                    AppRoutes.pushReplacementTo(context,
+                                        ViewPatients(therapistId: therapistId, name: element.data()['firstName'] +
+                                            ' ' +
+                                            element.data()['lastName'],));
+                                  } else {
+                                    AppRoutes.pushReplacementTo(context,
+                                         ViewPatients(therapistId: '',name: element.data()['firstName'] + ' ' + element.data()['lastName'],));
+                                  }
+                                } else {
+                                  AppRoutes.pushReplacementTo(
+                                      context, const AdminHome());
+                                }
+                              } else {
+                                AppLoading.show(context, AppMessage.loginTx,
+                                    AppMessage.userNotFound);
+                              }
+                            }
+                          });
+                        }
+                      });
+                    }
+                  },
+                )
+              ],
             ),
           ),
-          SizedBox(height: 10), SizedBox( width: 400.0,
-              height: 50.0,
-              child: AppTextFields(
-                controller: emailController,
-                labelText: AppMessage.emailTx,
-                validator: (v) => AppValidator.validatorEmail(v),
-                obscureText: false,
-              ),
-            ),
-
-          SizedBox(height: 10),
-
-            SizedBox(
-              width: 400.0,
-              height: 50.0,
-              child: AppButtons(
-                onPressed: passReset,
-                text: 'Reset password',
-              ),
-            ),
-
-        ],
+        ),
       ),
     );
-  }}
+  }
+}
+
