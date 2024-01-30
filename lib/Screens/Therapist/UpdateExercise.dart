b7d3e9c416ff2106f3614313da76781ea1f83a51
lujain-1 <128541474+lujain-1@users.noreply.github.com>
2024-01-30 08:28:46 +0300

    Update UpdateExercise.dart

--- lib/Screens/Therapist/UpdateExercise.dart
+++ lib/Screens/Therapist/UpdateExercise.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:intl/intl.dart';
+import 'package:multi_select_flutter/chip_display/multi_select_chip_display.dart';
 import 'package:physio/Database/Database.dart';
 import 'package:physio/Widget/AppButtons.dart';
 import 'package:physio/Widget/AppColor.dart';
@@ -10,12 +11,15 @@ import 'package:physio/Widget/AppLoading.dart';
 import 'package:physio/Widget/AppMessage.dart';
 import 'package:physio/Widget/AppTextFields.dart';
 
+import 'package:multi_select_flutter/dialog/multi_select_dialog_field.dart';
+import 'package:multi_select_flutter/util/multi_select_item.dart';
+
 import '../../Widget/AppBar.dart';
 
 class UpdateExercise extends StatefulWidget {
   final String exerciseId;
-
-  UpdateExercise({required this.exerciseId});
+final String patientId;
+  UpdateExercise({required this.exerciseId,required this.patientId});
 
   @override
   State<UpdateExercise> createState() => _UpdateExerciseState();
@@ -27,7 +31,10 @@ class _UpdateExerciseState extends State<UpdateExercise> {
   TextEditingController startDateController = TextEditingController();
   TextEditingController finishDateController = TextEditingController();
   TextEditingController durationController = TextEditingController();
-
+  TextEditingController PlanController = TextEditingController();
+  List<MultiSelectItem<Object?>> convertToMultiSelectItems(List<String> listItem) {
+    return listItem.map((item) => MultiSelectItem<Object?>(item, item)).toList();
+  }
   @override
   void initState() {
     super.initState();
@@ -36,12 +43,13 @@ class _UpdateExerciseState extends State<UpdateExercise> {
 
   void fetchExerciseDetails() async {
     try {
-      var exerciseDetails = await Database.getExerciseDetails(widget.exerciseId);
+      var exerciseDetails = await Database.getExerciseDetails(widget.exerciseId,widget.patientId);
       if (exerciseDetails != null) {
         setState(() {
           startDateController.text = exerciseDetails['startDate'];
           finishDateController.text = exerciseDetails['finishDate'];
           durationController.text = exerciseDetails['duration'];
+          PlanController.text = exerciseDetails['planName'];
           selectedExercise = exerciseDetails['exercise'];
         });
       }
@@ -72,7 +80,7 @@ class _UpdateExerciseState extends State<UpdateExercise> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarWidget(text:   'Update Exercise',
+      appBar: AppBarWidget(text:   'Update Plan',
 
       ),
       body: Form(
@@ -84,6 +92,7 @@ class _UpdateExerciseState extends State<UpdateExercise> {
               SizedBox(
                 height: 20.h,
               ),
+
               AppTextFields(
                 controller: startDateController,
                 labelText: 'start date',
@@ -172,6 +181,39 @@ class _UpdateExerciseState extends State<UpdateExercise> {
               SizedBox(
                 height: 10.h,
               ),
+              MultiSelectDialogField(
+                items: convertToMultiSelectItems(AppConstants.ExerciseList),
+                validator: (v) {
+                  if (v == null) {
+                    return AppMessage.mandatoryTx;
+                  } else {
+                    return null;
+                  }
+                },
+                onConfirm: (List<Object?>? selectedItems) {
+                  setState(() {
+                    selectedExercise = selectedItems?.cast<String>().join(", ");
+                  });
+                  print('selectedexercise: $selectedExercise');
+                },
+                buttonText: Text('Select Exercise'),
+                title: Text('Select Exercise'),
+                initialValue: selectedExercise?.split(", ")?.where((item) => item != null).map<Object?>((e) => e).toList() ?? [],
+                searchable: true,
+                selectedItemsTextStyle: TextStyle(fontSize: 14.0 ,color: Colors.black),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(6.0),
+
+                  color: Colors.white,
+                ),chipDisplay: MultiSelectChipDisplay(chipColor: Colors.white,textStyle: TextStyle(color: Colors.black)),
+                buttonIcon: Icon(
+                  Icons.arrow_drop_down,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(
+                height: 10.h,
+              ),
               AppButtons(
                 text: 'Update',
                 bagColor: AppColor.iconColor,
@@ -193,6 +235,7 @@ class _UpdateExerciseState extends State<UpdateExercise> {
     try {
       calculateDuration();
       await Database.updateExercise(widget.exerciseId, {
+        'planName':PlanController.text,
         'exercise': selectedExercise!,
         'startDate': startDateController.text,
         'finishDate': finishDateController.text,
@@ -207,4 +250,6 @@ class _UpdateExerciseState extends State<UpdateExercise> {
     }
   }
 }
+
+
 
