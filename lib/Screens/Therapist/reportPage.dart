e706975d4a8afab13f686ffc3140d139c7ccedea
lujain-1 <128541474+lujain-1@users.noreply.github.com>
2024-02-29 02:48:53 +0300

    Update reportPage.dart

--- lib/Screens/Therapist/reportPage.dart
+++ lib/Screens/Therapist/reportPage.dart
@@ -1,121 +1,264 @@
+
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import '../../../Database/Database.dart';
+import 'package:share_plus/share_plus.dart';
 import '../../../Widget/AppBar.dart';
-import '../../../Widget/AppColor.dart';
-import '../../../Widget/AppConstants.dart';
-import '../../../Widget/AppIcons.dart';
-import '../../../Widget/AppLoading.dart';
-import '../../../Widget/AppMessage.dart';
-import '../../../Widget/AppPopUpMen.dart';
-import '../../../Widget/AppRoutes.dart';
-import '../../../Widget/AppSize.dart';
-import '../../../Widget/AppText.dart';
-import '../../../Widget/generalWidget.dart';
-import 'package:physio/Screens/Account/Login.dart';
-import 'package:physio/Screens/Therapist/TherapistHome.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:physio/Screens/Patient/patientExercise.dart';
+import '../../Database/Database.dart';
+import '../../Widget/AppButtons.dart';
+import '../../Widget/AppIcons.dart';
 import '../../Widget/AppImage.dart';
-import '../Patient/ChangePass.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
 
-class ViewPatients extends StatefulWidget {
-  final String therapistId;
-  final String name;
-   ViewPatients({Key? key, required this.therapistId, required this.name}) : super(key: key);
+import 'package:pdf/widgets.dart' as pw;
+
+import 'package:physio/Screens/Therapist/TherapistHome.dart';
+import '../../../Widget/AppBar.dart';
+import '../../../Widget/AppMessage.dart';
+import '../../Widget/AppColor.dart';
+import '../../Widget/AppConstants.dart';
+import '../../Widget/AppPopUpMen.dart';
+import '../../Widget/AppRoutes.dart';
+import '../../Widget/AppSize.dart';
+import '../../Widget/AppText.dart';
+import '../../Widget/AppTextFields.dart';
+import '../../Widget/AppValidator.dart';
+import '../../Widget/GeneralWidget.dart';
+import '../Account/Login.dart';
+
+
+import '../../../Widget/AppMessage.dart';
+class reportPage extends StatefulWidget {
+  final String planId;
+  final String userId;
+  final String level;
+  final String exercise;
+  final String date;
+
+  const reportPage({Key? key, required this.planId, required this.userId,required this.level,required this.exercise,required this.date}) : super(key: key);
 
   @override
-  State<ViewPatients> createState() => _ViewTherapistState();
+  State<reportPage> createState() => _reportPageState();
 }
-String selectedUser='';
-class _ViewTherapistState extends State<ViewPatients> {
-  late ImageProvider exerciseImg= AssetImage(AppImage.pro);
+
+late ImageProvider exerciseImg = AssetImage(AppImage.scoring);
+
+class _reportPageState extends State<reportPage> {
+  Set<String> displayedDates = Set<String>();
+
+  String  planName='';
+  int total=0;
+
+
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPlanName();
+  }
+
+  Future<void> fetchPlanName() async {
+    final planSnapshot = await FirebaseFirestore.instance.collection('plan').doc(widget.planId).get();
+    final planData = planSnapshot.data() as Map<String, dynamic>?;
+    if (planData != null) {
+      setState(() {
+        planName = planData['planName'];
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarWidget(
-          text: AppMessage.PatientList,
-          leading: AppPopUpMen(
-              icon: CircleAvatar(
-                backgroundColor: AppColor.black,
-                child: Icon(AppIcons.menu),
-              ),
-              menuList: AppWidget.itemList(
-                  onTapChangePass: () =>
-                      AppRoutes.pushTo(context, const ChangePass()),
-                  isChangePassword: true,
-                  helloName: 'Hello Therapist ${widget.name}',
-                  action: () {
-                    Database.logOut();
-                    AppRoutes.pushReplacementTo(context, const Login());
-                  }))),
-      body: StreamBuilder(
-        stream: AppConstants.userCollection
-            .where('type', isEqualTo: AppConstants.typeIsPatient)
-            .where('therapistId', isEqualTo: widget.therapistId)
-         .where('activeUser', isEqualTo: true)
-            .snapshots(),
-        builder: (context, AsyncSnapshot snapshot) {
+      appBar: AppBarWidget(text: 'Report Page'),
+      body: StreamBuilder<DocumentSnapshot>(
+        stream: FirebaseFirestore.instance.collection('reports').doc(widget.userId).snapshots(),
+        builder: (context, snapshot) {
           if (snapshot.hasError) {
             return Center(child: Text('${snapshot.error}'));
           }
-          if (snapshot.hasData) {
-            return body(context, snapshot);
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
           }
-          return const Center(
-            child: CircularProgressIndicator(),
+
+          final reportData = snapshot.data?.data() as Map<String, dynamic>?;
+
+          if (reportData == null || reportData.isEmpty) {
+            return Center(
+              child: Text(
+                'No data',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            );
+          }
+
+          final exercises = reportData.entries
+              .where((entry) => entry.key != 'score')
+              .map((entry) => entry.value)
+              .toList();
+
+          if (exercises.isEmpty) {
+            return Center(
+              child: Text(
+                'No exercises found',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            );
+          }
+
+          final filteredExercises = exercises.where((score) {
+            return score['planId'] == widget.planId &&
+                score['exercise'] == widget.exercise &&
+                score['level'] == widget.level &&
+                score['date'] == widget.date;
+          }).toList();
+total=filteredExercises.length;
+          if (filteredExercises.isEmpty) {
+            return Center(
+              child: Text(
+                'No data found for the specified exercise',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            );
+          }
+
+          final scores = filteredExercises.map((exerciseData) => exerciseData['score']).toList();
+          final scoresString = scores.join(', ');
+
+          return SingleChildScrollView(
+            child: Center(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.1),
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 100,
+                        child: ClipOval(
+                          child: Image(
+                            image: exerciseImg,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 40),
+                AppTextFields(
+                  controller: TextEditingController(text: planName),
+                  labelText: 'Plan Name',
+                  validator: (v) => AppValidator.validatorName(v),
+                  obscureText: false,
+                  enable: false,
+                ),
+                SizedBox(height: 10.h),
+                AppTextFields(
+                  controller: TextEditingController(text: widget.exercise),
+                  labelText: 'Exercise Type',
+                  validator: (v) => AppValidator.validatorName(v),
+                  obscureText: false,
+                  enable: false,
+                ),
+                SizedBox(height: 10.h),
+                AppTextFields(
+                  controller: TextEditingController(text: widget.level),
+                  labelText: 'Exercise Level',
+                  validator: (v) => AppValidator.validatorEmpty(v),
+                  obscureText: false,
+                  enable: false,
+                ),
+                SizedBox(height: 10.h),
+
+                AppTextFields(
+                  controller: TextEditingController(text: widget.date),
+                  labelText: 'Exercise Date',
+                  validator: (v) => AppValidator.validatorEmpty(v),
+                  obscureText: false,
+                  enable: false,
+                ),
+                SizedBox(height: 10.h),
+                      AppTextFields(
+                        controller: TextEditingController(text: total.toString()),
+                        labelText: 'total exercise ',
+                        validator: (v) => AppValidator.validatorEmpty(v),
+                        obscureText: false,
+                        enable: false,
+                      ),
+                      SizedBox(height: 10.h),
+                AppTextFields(
+                  controller: TextEditingController(text: scoresString),
+                  labelText: 'Scores',
+                  validator: (v) => AppValidator.validatorEmpty(v),
+                  obscureText: false,
+                  enable: false,
+                ),
+                      SizedBox(height: 20.h),
+                      ElevatedButton(
+                        onPressed: () {
+                          _shareAsPdf(scoresString, total);
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(AppColor.black),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('Share File '),
+                            Icon(AppIcons.share),
+                          ],
+                        ),
+                      ),
+
+              ],
+            ),
+          ),
+          ),
+          );
+        },
+      ),
+    );}
+  Future<void> _shareAsPdf(String scoresString, int total) async {
+    final pdf = pw.Document();
+
+    pdf.addPage(
+      pw.Page(
+        build: (pw.Context context) {
+          return pw.Center(
+            child: pw.Column(
+              mainAxisAlignment: pw.MainAxisAlignment.center,
+              crossAxisAlignment: pw.CrossAxisAlignment.center,
+              children: [
+                pw.Text('Plan Name: $planName', style: pw.TextStyle(fontSize: 20)),
+                pw.SizedBox(height: 10),
+                pw.Text('Exercise Type: ${widget.exercise}', style: pw.TextStyle(fontSize: 20)),
+                pw.SizedBox(height: 10),
+                pw.Text('Exercise Level: ${widget.level}', style: pw.TextStyle(fontSize: 20)),
+                pw.SizedBox(height: 10),
+                pw.Text('Date: ${widget.date}', style: pw.TextStyle(fontSize: 20)),
+                pw.SizedBox(height: 10),
+                pw.Text('Total: $total', style: pw.TextStyle(fontSize: 20)),
+                pw.SizedBox(height: 10),
+                pw.Text('Scores: $scoresString', style: pw.TextStyle(fontSize: 20)),
+              ],
+            ),
           );
         },
       ),
     );
-  }
 
-  Widget body(context, snapshot) {
-    return snapshot.data.docs.length > 0
-        ? ListView.builder(
-      itemCount: snapshot.data.docs.length,
-      itemBuilder: (context, i) {
-        var data = snapshot.data.docs[i].data();
-        return Padding(
-          padding: EdgeInsets.symmetric(vertical: 5.h),
-          child: SizedBox(
-            height: 100.h,
-            width: double.maxFinite,
-            child: Card(
-              elevation: 5,
-              child: Center(
-                child: ListTile(
-                  onTap:() {
-                    setState((){
-                      selectedUser=snapshot.data.docs[i].data()['userId'];});
-                    AppRoutes.pushTo(context,TherapistHome(PatientId:selectedUser));},
+    final bytes = await pdf.save();
 
-                  tileColor: AppColor.white,
-                  leading: CircleAvatar(
-                    radius: 25.0,
-                    child: ClipOval(
-                      child: Image(
-                        image: exerciseImg,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                  title: AppText(
-                    text: data['firstName'] + ' ' + data['lastName'],
-                    fontSize: AppSize.subTextSize,
-                  ),
-                ),
-              ),
-            ),
-          ),
-        );
-      },
-    )
-        : Center(
-      child: AppText(
-        text: AppMessage.noData,
-        fontSize: AppSize.subTextSize,
-        fontWeight: FontWeight.bold,
-      ),
-    );
-  }
-}
+    Printing.sharePdf(bytes: bytes, filename: 'exercise_report.pdf');
+  }}
